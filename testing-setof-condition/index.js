c5dd0b825056704a7fcd4aac9b479e34d15cd947
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-07-21 15:56:57 -0700

    done with set of conditions

---
+++ testing-setof-condition/index.js
@@ -0,0 +1,37 @@
+// CHAPTER-13 TESTING SETS (MULTIPLE) OF CONDITION
+
+// ABOUT RAIN
+// if (badal) {
+//   alert("it's gonna be raining soon in peshawar");
+// }else {
+//     alert("no chance of rain in peshawar");
+// }
+
+// FIND LARGEST OF THREE NUMBERS
+// const firtNumber = +prompt("enter first number");
+// const secondNumber = +prompt("enter second number");
+// const thirdNumber = +prompt("enter third number");
+
+// if (firtNumber > secondNumber && firtNumber > thirdNumber) {
+//   console.log("first number that is " + firtNumber + "is the largest number");
+// } else if (secondNumber > thirdNumber) {
+//     console.log("second number that is " + secondNumber + "is the largest number");
+// } else {
+//   console.log("third number that is " + thirdNumber + "is the largest number");
+// }
+
+// GRADING SYSTEM
+// const userName = prompt("please enter your name");
+// const score = +prompt("please enter your score");
+
+// if(score >= 90) {
+//   alert(userName + "your Grade is A");
+// } else if (score >= 80) {
+//     alert(userName + "your Grade is B");
+// } else if (score >= 70) {
+//     alert(userName + "your Grade is c");
+// } else if (score >= 60) {
+//     alert(userName + "your Grade is D");
+// } else {
+//     alert(userName + "your Grade is F");
+// }
